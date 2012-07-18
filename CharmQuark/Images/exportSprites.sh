788810218e8fe36d3a155e1915cd41d2fce94f82
Nathan Babb <nathan.babb@aethertheory.com>
2012-07-18 13:34:20 -0400

    New sprites and export tool.

---
+++ CharmQuark/Images/exportSprites.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+INPUT_FILE=SpriteWorksheet.svg
+
+#INKSCAPE=/Applications/Inkscape.app/Contents/Resources/script
+INKSCAPE=/Applications/Inkscape.app/Contents/Resources/bin/inkscape
+
+EXPORT_DIR=Sprites
+IPAD_DIR=$EXPORT_DIR/iPad
+IPAD_DPI=90
+IPADHD_DIR=$EXPORT_DIR/iPad-hd
+IPADHD_DPI=180
+IPHONE_DIR=$EXPORT_DIR/iPhone
+IPHONE_DPI=42.19
+IPHONEHD_DIR=$EXPORT_DIR/iPhone-hd
+IPHONEHD_DPI=84.38
+
+SPRITE_IDS='red red-small green green-small blue blue-small antired antired-small antigreen antigreen-small antiblue antiblue-small white white-small lhcmap pause detector'
+
+mkdir -p $IPAD_DIR $IPADHD_DIR $IPHONE_DIR $IPHONEHD_DIR
+
+export LANG=C
+
+for i in $SPRITE_IDS; do
+$INKSCAPE --export-png=$IPAD_DIR/$i.png --export-dpi=$IPAD_DPI --export-id=$i --export-id-only $INPUT_FILE
+$INKSCAPE --export-png=$IPADHD_DIR/$i.png --export-dpi=$IPADHD_DPI --export-id=$i --export-id-only $INPUT_FILE
+$INKSCAPE --export-png=$IPHONE_DIR/$i.png --export-dpi=$IPHONE_DPI --export-id=$i --export-id-only $INPUT_FILE
+$INKSCAPE --export-png=$IPHONEHD_DIR/$i.png --export-dpi=$IPHONEHD_DPI --export-id=$i --export-id-only $INPUT_FILE
+done;
