6da64b2a813c8e2fd796c4763569a3c229226c6a
Nathan Babb <nathan.babb@aethertheory.com>
2012-09-27 14:23:19 -0400

    Log into game center.

--- CharmQuark/Classes/AppDelegate.m
+++ CharmQuark/Classes/AppDelegate.m
@@ -113,6 +113,9 @@
 	
     // Start Audio
     [[GameManager sharedGameManager] setupAudioEngine];
+
+    // Log into gamecenter.
+    [[GCHelper sharedInstance] authenticateLocalUser];
     
 	return YES;
 }
